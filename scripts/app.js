0696b346a22bdd395bb23bb5953c5c3f73869cc1
mfriedoony12 <mfriedoony@gmail.com>
2024-12-19 19:28:56 +0330

    update Nav

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,17 @@
-const toggleThemeBtn = document.querySelector("#toggle-theme");
-toggleThemeBtn.addEventListener("click" , () => {
-    if (localStorage.theme === "dark"){
-        document.documentElement.classList.remove("dark");
-        localStorage.theme = "light";
-    } else {
-        document.documentElement.classList.add("dark");
-        localStorage.setItem("theme" , "dark");
-    }
+const toggleThemeBtns = document.querySelectorAll(".toggle-theme");
+
+
+toggleThemeBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        if (localStorage.theme === "dark"){
+            document.documentElement.classList.remove("dark");
+            localStorage.theme = "light";
+        } else {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("theme" , "dark");
+        }
+    })
+})
+toggleThemeBtns.addEventListener("click" , () => {
+
 })
