c121abfd9fe665da62c7a3305699a655783aec18
mfriedoony12 <mfriedoony@gmail.com>
2024-12-20 03:07:46 +0330

    complete mobile-nav

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,13 @@
 const toggleThemeBtns = document.querySelectorAll(".toggle-theme");
-
+const submenuOpenBtn = document.querySelector(".submenu-open-btn");
+const submenu = document.querySelector(".submenu");
+const navOpenBtn = document.querySelector(".nav-icon");
+const navCloseBtn = document.querySelector(".nav-close-btn");
+const nav = document.querySelector(".nav");
+const overlay = document.querySelector(".overlay");
 
 toggleThemeBtns.forEach(btn => {
-    btn.addEventListener("click", () => {
+    btn.addEventListener("click", function (){
         if (localStorage.theme === "dark"){
             document.documentElement.classList.remove("dark");
             localStorage.theme = "light";
@@ -12,6 +17,25 @@ toggleThemeBtns.forEach(btn => {
         }
     })
 })
-toggleThemeBtns.addEventListener("click" , () => {
+submenuOpenBtn.addEventListener("click", (e) => {
+    e.currentTarget.parentElement.classList.toggle("text-orange-300");
+    submenu.classList.toggle("submenu--open");
+    submenuOpenBtn.classList.toggle("rotate-180");
+})
+navOpenBtn.addEventListener("click",() => {
+    nav.classList.remove("-right-64");
+    nav.classList.add("right-0");
+    overlay.classList.add("overlay--visible");
 
 })
+
+
+function closeNav() {
+    nav.classList.remove("right-0");
+    nav.classList.add("-right-64");
+    overlay.classList.remove("overlay--visible");
+}
+
+navCloseBtn.addEventListener("click",closeNav)
+overlay.addEventListener("click",closeNav)
+
